6aac903206c64e0c1455a22442a648624cc3f16a
ClaudHelloworld <fantasmagomes999@gmail.com>
2020-09-03 08:47:22 +0900

    Atualização 2

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,16 +1,23 @@
 // This is a JavaScript file
 
-$(document).on("click","#somar",function(){
-    var valor1 = parseFloat($("#valor1").val());
-
-  if (valor1  <= 1200){
-    var cal = 1200 - valor1;
-    var resultado = "Você ainda precisa ou, já está no seu limite de calorias, Restantes = " +  cal ; 
-  }
-
-  else{
-    var resultado = "Você já Ultrapassou as 1200kg Diárias Recomendadas";
-  }
+$(document).on("click",'#somar',function(){
   
-    $("#resultado").text(resultado);
+     var sexo = parseFloat($("#option:selected", ("#sexo")).val());
+     var idade = parseFloat($("#idade").val());
+     var altura = parseFloat($("#altura").val());
+     var peso = parseFloat($("#peso").val());
+     var gp = parseFloat($("#option:selected", ("#gp")).val());
+
+if(sexo=="M"){
+  var resultado = gp * (66+( (13.7 * peso) + (5* altura) - (6.8 * idade) ) );
+}
+else{
+  var resultado = gp * (655+ ( (9.6* peso) + (1.8* altura) - (4.7 * idade) ) );
+}
+ 
+ 
+
+    $("#resultado").val(resultado.toFixed(2));
+    console.log(resultado);
 });
+
